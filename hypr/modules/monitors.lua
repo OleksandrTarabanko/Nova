e2ca8f0fb403bf16161c57b45687084d66e089e7
Oleksandr Tarabanko <olexandrtarabanko@gmail.com>
2026-05-24 17:06:29 +0200

    hypr config + waybar config

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,17 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "auto",
+})
+
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
