e19acfe58a85a8fc835d1917ff5397b88d5cb479
Oleksandr Tarabanko <olexandrtarabanko@gmail.com>
2026-06-01 14:11:14 +0200

    changed Cursor

--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -4,7 +4,9 @@
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
+hl.env("XCURSOR_THEME", "Bibata-Original-Classic")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Bibata-Original-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 -- Toolkit Backend
